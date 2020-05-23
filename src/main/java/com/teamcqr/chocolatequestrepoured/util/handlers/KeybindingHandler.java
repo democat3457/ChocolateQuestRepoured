8135c1bee339639befef813b7db878a4f9dfb184
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-05-23 13:19:11 +0200

    updated to 1.15.2

--- src/main/java/com/teamcqr/chocolatequestrepoured/util/handlers/KeybindingHandler.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/util/handlers/KeybindingHandler.java
@@ -3,15 +3,15 @@ package com.teamcqr.chocolatequestrepoured.util.handlers;
 import com.teamcqr.chocolatequestrepoured.proxy.ClientProxy;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.PlayerEntitySP;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.InputEvent.KeyInputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@OnlyIn(Side.CLIENT)
-@EventBusSubscriber(value = Side.CLIENT)
+@OnlyIn(Dist.CLIENT)
+@EventBusSubscriber
 public class KeybindingHandler {
 
 	// Opens repu GUI
@@ -19,7 +19,7 @@ public class KeybindingHandler {
 	public static void onKeyPress(KeyInputEvent event) {
 		// Repu key
 		if (ClientProxy.keybindReputationGUI.isPressed()) {
-			PlayerEntitySP player = Minecraft.getMinecraft().player;
+			ClientPlayerEntity player = Minecraft.getInstance().player;
 			// player.openGui(CQRMain.INSTANCE, Reference.REPUTATION_GUI_ID, Minecraft.getMinecraft().world, (int)player.posX, (int)player.posY, (int)player.posZ);
 		}
 	}
