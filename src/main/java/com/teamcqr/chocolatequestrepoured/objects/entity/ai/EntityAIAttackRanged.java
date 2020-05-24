d1b472a402d57251a42bf89e198abf48d5dcf38a
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-05-24 11:10:44 +0200

    updated to 1.15.2

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/EntityAIAttackRanged.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/ai/EntityAIAttackRanged.java
@@ -5,13 +5,15 @@ import com.teamcqr.chocolatequestrepoured.objects.entity.bases.AbstractEntityCQR
 import com.teamcqr.chocolatequestrepoured.util.IRangedWeapon;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.item.ArrowItem;
+import net.minecraft.item.BowItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemArrow;
-import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.Vec3d;
 
 public class EntityAIAttackRanged extends EntityAIAttack {
 
@@ -48,7 +50,7 @@ public class EntityAIAttackRanged extends EntityAIAttack {
 	}
 
 	@Override
-	public void updateTask() {
+	public void tick() {
 		LivingEntity attackTarget = this.entity.getAttackTarget();
 
 		if (attackTarget != null) {
@@ -72,26 +74,30 @@ public class EntityAIAttackRanged extends EntityAIAttack {
 	protected void checkAndPerformAttack(LivingEntity attackTarget) {
 		if (this.attackTick <= 0 && this.entity.getDistance(attackTarget) <= 32.0D) {
 			ItemStack stack = this.entity.getHeldItemMainhand();
-			if (stack.getItem() instanceof ItemBow) {
+			if (stack.getItem() instanceof BowItem) {
 				this.attackTick = 60;
 				ItemStack arrowItem = this.entity.getItemStackFromExtraSlot(EntityEquipmentExtraSlot.ARROW);
-				if (arrowItem.isEmpty() || !(arrowItem.getItem() instanceof ItemArrow)) {
+				if (arrowItem.isEmpty() || !(arrowItem.getItem() instanceof ArrowItem)) {
 					arrowItem = new ItemStack(Items.ARROW, 1);
 				}
-				EntityArrow arrow = ((ItemArrow) arrowItem.getItem()).createArrow(this.entity.world, arrowItem, this.entity);
+				AbstractArrowEntity arrow = ((ArrowItem) arrowItem.getItem()).createArrow(this.entity.world, arrowItem, this.entity);
 				arrowItem.shrink(1);
 
-				double x = attackTarget.posX - this.entity.posX;
-				double y = attackTarget.posY + (double) attackTarget.height * 0.5D - arrow.posY;
-				double z = attackTarget.posZ - this.entity.posZ;
+				double x = attackTarget.getPosX() - this.entity.getPosX();
+				double y = attackTarget.getPosY() + (double) attackTarget.getHeight() * 0.5D - arrow.getPosY();
+				double z = attackTarget.getPosZ() - this.entity.getPosZ();
 				double distance = Math.sqrt(x * x + z * z);
 				arrow.shoot(x, y + distance * 0.06D, z, 3.0F, 0.0F);
-				arrow.motionX += this.entity.motionX;
-				arrow.motionZ += this.entity.motionZ;
+				double vx = arrow.getMotion().x;
+				double vy = arrow.getMotion().y;
+				double vz = arrow.getMotion().z;
+				vx += this.entity.getMotion().x;
+				vz += this.entity.getMotion().z;
 				if (!this.entity.onGround) {
-					arrow.motionY += this.entity.motionY;
+					vy += this.entity.getMotion().y;
 				}
-				this.entity.world.spawnEntity(arrow);
+				arrow.setMotion(new Vec3d(vx,vy,vz));
+				this.entity.world.addEntity(arrow);
 				this.entity.playSound(SoundEvents.ENTITY_SKELETON_SHOOT, 1.0F, 0.8F + this.random.nextFloat() * 0.4F);
 			} else if (stack.getItem() instanceof IRangedWeapon) {
 				IRangedWeapon weapon = (IRangedWeapon) stack.getItem();
@@ -103,7 +109,7 @@ public class EntityAIAttackRanged extends EntityAIAttack {
 	}
 
 	protected boolean isRangedWeapon(Item item) {
-		return item instanceof ItemBow || item instanceof IRangedWeapon;
+		return item instanceof BowItem || item instanceof IRangedWeapon;
 	}
 
 }
