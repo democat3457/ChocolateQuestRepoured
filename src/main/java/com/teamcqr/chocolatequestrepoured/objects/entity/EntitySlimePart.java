11be5cc8eacad15bc0726c3bbe143452fc628449
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2020-05-31 15:36:51 +0200

    updated to 1.15.2

--- src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/EntitySlimePart.java
+++ src/main/java/com/teamcqr/chocolatequestrepoured/objects/entity/EntitySlimePart.java
@@ -3,10 +3,10 @@ package com.teamcqr.chocolatequestrepoured.objects.entity;
 import java.util.UUID;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
-import net.minecraft.entity.monster.EntitySlime;
 import net.minecraft.entity.monster.SlimeEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.NBTUtil;
@@ -16,42 +16,42 @@ public class EntitySlimePart extends SlimeEntity {
 
 	private UUID ownerUuid;
 
-	public EntitySlimePart(World worldIn) {
-		super(worldIn);
+	public EntitySlimePart(World worldIn, EntityType<? extends EntitySlimePart> type) {
+		super(type, worldIn);
 	}
 
-	public EntitySlimePart(World worldIn, LivingEntity owner) {
-		this(worldIn);
+	public EntitySlimePart(World worldIn, LivingEntity owner, EntityType<? extends EntitySlimePart> type) {
+		this(worldIn, type);
 		this.ownerUuid = owner.getUniqueID();
 	}
 
 	@Override
-	protected void initEntityAI() {
-		super.initEntityAI();
-		this.targetTasks.taskEntries.clear();
+	protected void registerGoals() {
+		super.registerGoals();
+		this.target.taskEntries.clear();
 	}
 
 	@Override
-	protected void applyEntityAttributes() {
-		super.applyEntityAttributes();
-		IAttributeInstance iattributeinstance = this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED);
+	protected void registerAttributes() {
+		super.registerAttributes();
+		IAttributeInstance iattributeinstance = this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED);
 		iattributeinstance.setBaseValue(iattributeinstance.getBaseValue() * 0.5D);
 	}
 
 	@Override
-	public void onUpdate() {
+	public void tick() {
 		if (this.ticksExisted > 400) {
-			this.setDead();
+			this.remove();
 		}
 
-		super.onUpdate();
+		super.tick();
 	}
 
 	@Override
 	protected void collideWithEntity(Entity entityIn) {
 		if (entityIn instanceof LivingEntity && entityIn.getUniqueID().equals(this.ownerUuid)) {
 			((LivingEntity) entityIn).heal(2.0F);
-			this.setDead();
+			this.remove();
 		}
 	}
 
@@ -61,17 +61,17 @@ public class EntitySlimePart extends SlimeEntity {
 	}
 
 	@Override
-	public void writeEntityToNBT(CompoundNBT compound) {
-		super.writeEntityToNBT(compound);
-		compound.setInteger("ticksExisted", this.ticksExisted);
-		compound.setTag("ownerUuid", NBTUtil.createUUIDTag(this.ownerUuid));
+	public void writeAdditional(CompoundNBT compound) {
+		super.writeAdditional(compound);
+		compound.putInt("ticksExisted", this.ticksExisted);
+		compound.put("ownerUuid", NBTUtil.writeUniqueId(this.ownerUuid));
 	}
 
 	@Override
-	public void readEntityFromNBT(CompoundNBT compound) {
-		super.readEntityFromNBT(compound);
-		this.ticksExisted = compound.getInteger("ticksExisted");
-		this.ownerUuid = NBTUtil.getUUIDFromTag(compound.getCompoundTag("ownerUuid"));
+	public void readAdditional(CompoundNBT compound) {
+		super.readAdditional(compound);
+		this.ticksExisted = compound.getInt("ticksExisted");
+		this.ownerUuid = NBTUtil.readUniqueId(compound.getCompound("ownerUuid"));
 	}
 
 }
